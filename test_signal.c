ab0eb74925278389a6eaf397e187593103f7ec96
wrt <wrt5368@foxmail.com>
2021-11-24 23:20:52 +0800

    branch-test

--- test_signal.c
+++ test_signal.c
@@ -7,6 +7,8 @@ void f(int signal_no) {
 	printf("got a signal: %d\n", signal_no);
 }
 
+// for test
+
 int main(int argc, char *argv[]) {
 	while (1) {
 		signal(2, f);		// 将收到2号信号后的动作改为执行函数f，而不是终止进程
