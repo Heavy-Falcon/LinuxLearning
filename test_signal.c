40e4a6316907f7163f9c90bf905fedbcff07a0ce
wrt <wrt5368@foxmail.com>
2021-11-24 22:23:55 +0800

    signal函数

---
+++ test_signal.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+
+void f(int signal_no) {
+	printf("got a signal: %d\n", signal_no);
+}
+
+int main(int argc, char *argv[]) {
+	while (1) {
+		signal(2, f);		// 将收到2号信号后的动作改为执行函数f，而不是终止进程
+		sleep(1);
+	}
+	return 0;
+}
